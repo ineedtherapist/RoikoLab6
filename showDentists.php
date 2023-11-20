32223c29c968827af58af99557e3b88834b884ed
ineedtherapist <viktorkovalchuk012@gmail.com>
2023-11-21 00:19:27 +0200

    showDentists

--- showDentists.php
+++ showDentists.php
@@ -3,11 +3,11 @@
     <meta charset="utf-8">
     <meta name="keywords" content="Лабораторна робота, MySQL, робота з базою даних">
     <meta name="description" content="Лабораторна робота. Робота з базою даних">
-    <title>Таблиця Clinic</title>
+    <title>Таблиця Dentists</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <h1>Таблиця Clinic</h1>
+    <h1>Таблиця Dentists</h1>
 
 
     <?php
@@ -15,12 +15,12 @@
     include "databaseConnect.php";
 
     try {
-        $stmt = $pdo->query("SELECT * FROM groups");
+        $stmt = $pdo->query("SELECT * FROM dentists");
         // Виконання запиту і отримання результатів
-        printf("<h3>Список груп:</h3>");
-        printf("<table><tr><th>ІД Групи</th><th>Назва групи</th></tr>");
+        printf("<h3>Список Лікарів:</h3>");
+        printf("<table><tr><th>ID</th><th>name</th><th>lastname</th><th>middlename</th><th>post</th><th>clinic_id</th><th>phone_number</th><th>date_birth</th><th>hiring_date</th></tr>");
         while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-            printf("<tr><td>%s</td><td>%s</td></tr>", $row['id'], $row['name']);
+            printf("<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>", $row['id'], $row['name'], $row['lastname'], $row['middlename'], $row['post'], $row['clinic_id'], $row['phone_number'], $row['date_birth'], $row['hiring_date'] );
         };
         printf("</table>");
     } catch (PDOException $e) {

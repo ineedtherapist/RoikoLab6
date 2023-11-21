f2d725d8c87e1bffc6099c72d8fb150e4c6e4a07
ineedtherapist <viktorkovalchuk012@gmail.com>
2023-11-21 13:44:36 +0200

    update appoint

--- updateAppointments.php
+++ updateAppointments.php
@@ -2,15 +2,15 @@
 
 include "databaseConnect.php";
 
-$id = $_POST['update_id'];
+$app_id = $_POST['update_id'];
 $appoint_time = $_POST['update_appoint_time'];
 $dentist_id = $_POST['update_dentist_id'];
-$stmt = $pdo->prepare("UPDATE appointments SET id=:app_id, appoint_time=:appoint_time, dentist_id=:dentist_id WHERE id=:app_id );
+$stmt = $pdo->prepare("UPDATE appointments SET id=:id, appoint_time=:appoint_time, dentist_id=:dentist_id WHERE id=:id");
 $stmt->bindParam(':id', $app_id);
 $stmt->bindParam(':appoint_time', $appoint_time);
 $stmt->bindParam(':dentist_id', $dentist_id);
 if ($stmt->execute()) {
-    printf("Запис оновлено успішно!");
+    echo "Запис оновлено успішно!";
 } else {
     echo "Помилка оновлення запису";
 }
